c25edb13e0139fb9fab527d1f036c8bd4fbd7d30
Shan Kim <shankim35@gmail.com>
2023-01-30 11:08:56 -0500

    phase:0

---
+++ 01_knight_travails.rb
@@ -0,0 +1,18 @@
+require_relative "00_tree_node.rb"
+
+class KnightPathFinder
+
+    def initialize(start_pos)
+        @start_pos = start_pos
+        
+    end
+
+    def self.root_node
+        
+    end
+
+    def build_move_tree
+
+    end
+
+end
